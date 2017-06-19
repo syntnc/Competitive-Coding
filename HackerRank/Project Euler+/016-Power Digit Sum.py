40d3ad1bdc8e98fdea95e148a77045ae477bb5a1
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2017-06-19 14:05:19 +0530

    Minor deletion

--- HackerRank/Project Euler+/016-Power Digit Sum.py
+++ HackerRank/Project Euler+/016-Power Digit Sum.py
@@ -1,15 +1,3 @@
 t = int(input())
 for _ in range(t):
     print(sum(map(int, list(str(2 ** int(input()))))))
-
-
-# EXPANDED
-
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    l = list(str(2 ** n))
-    total = 0
-    for i in l:
-        total += int(i)
-    print(total)
